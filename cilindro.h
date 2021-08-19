76a968ecc855474d7410f27c1bf4b8962d76bc94
Rómulo Jesús Torres Llivipuma <71521545+romulo24@users.noreply.github.com>
2021-08-19 14:14:43 -0500

    Update cilindro.h

--- cilindro.h
+++ cilindro.h
@@ -1,3 +1,10 @@
+/*
+* Programacion Orientada a Objetos.
+* Autor: Estudiante Torres LLivipuma Romulo Jesus <rtorresll@est.ups.edu.ec>
+* Universidad Politecnica Salesiana
+* Fecha: 15-04-2021
+* Cilindro
+*/
 #ifndef CILINDRO_H
 #define CILINDRO_H
 #include"circulo.h"
